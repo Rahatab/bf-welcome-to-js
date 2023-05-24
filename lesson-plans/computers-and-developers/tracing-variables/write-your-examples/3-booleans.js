ced0e2c4967623ab9945a3b704683a26509c7d93
Rahatab <rahy_1987@hotmail.com>
2023-05-24 13:06:17 +0200

    updates-exc.

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/3-booleans.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/3-booleans.js
@@ -2,7 +2,40 @@
 
 /* Booleans
 
-TODO: create a few variables(boolean):
-- you need to add comments to show where the variable been
-declared, initialized, read , reassigned,...
+  1- booleans are a data type used to work with true or false( yes or no)
+
+  2- it can have only two values : true or false
+
+  3- booleans are very useful in comparisons and conditions
 */
+// decalre the varaiable isLoggedIn and initallize it to false
+let isLoggedIn = false;
+
+// decalre the varaiable isAuthorized and initallize it to true
+let isAuthorized = true;
+
+// decalre the varaiable hasAccess and initallize it to false
+let hasAccess = false;
+
+
+//read the value of isLoggedIn
+if (isLoggedIn) {
+  console.log('You can log in!');
+} else {
+  console.log("Sorry you can't log in");
+}
+
+//read the value of isAuthorized
+if (isAuthorized) {
+  console.log('You can Authorized to log in !');
+} else {
+  console.log("Sorry you aren't Authorized log in");
+}
+
+
+//read the value of hasAccess
+if (hasAccess) {
+  console.log('You can Access!');
+} else {
+  console.log("Sorry you can't Access");
+}
